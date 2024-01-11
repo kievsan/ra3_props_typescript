056ee206fe14b4b76779b3ad0e1b592ccdd73504
kievskiy <knhel7@mail.ru>
2024-01-11 12:44:23 +0300

    refactored a bit goods Listing component

--- src/components/listing/Listing.tsx
+++ src/components/listing/Listing.tsx
@@ -11,8 +11,20 @@ export default function Listing(props: ListingProps) {
     const truncateString = (str: string, maxlength: number): string => {
         return (str.length >= maxlength) ? str.slice(0, maxlength-1)+'...' : str;
     }
-    const { items } = props;  
+
+    const { items } = props;
+
+    const itemPrice = (currency_code: string, price: string): string => {
+        return currency_code === 'USD' ? `$${price}` : (
+            currency_code === 'EUR' ? `€${price}` : (
+                `${price} ${currency_code}`))
+    }
+    const quantityLevel = (quantity: number): string => {
+        return quantity <= 10 ? "level-low" : (
+            quantity <= 20 ? "level-medium" : "level-high")
+    }
     const filteredItems = items.filter( item => item.title);
+    
     const dataItems = filteredItems.map( item =>
         (
             <div className={classes["item"]} key={item.listing_id}>
@@ -22,27 +34,35 @@ export default function Listing(props: ListingProps) {
                     </a>
                 </div>
                 <div className={classes["item-details"]}>
-                    <p className={classes["item-title"]}>{item.title && truncateString(item.title,50)}</p>          
+                    <p className={classes["item-title"]}>
+                        {item.title && truncateString(item.title,50)}
+                    </p>
+                    <p className={classes["item-price"]}>
+                        {item.price && itemPrice(item.currency_code, item.price)}
+                    </p>
+                    <p className={`${classes["item-quantity"]} ${classes[quantityLevel(item.quantity)]}`}>
+                        {item.quantity} left
+                    </p>
                     
-                    { item.currency_code === 'USD' 
+{/*                     { item.currency_code === 'USD' 
                         ? <p className={classes["item-price"]}>${item.price}</p>
                         : ( item.currency_code === 'EUR'                
                             ? <p className={classes["item-price"]}>€{item.price}</p>
                             : <p className={classes["item-price"]}>{item.price} {item.currency_code}</p>
                         )
-                    }
+                    } */}
 
-                    { item.quantity && item.quantity <= 10
+{/*                     { item.quantity && item.quantity <= 10
                         ? <p className={`${classes["item-quantity"]} ${classes["level-low"]}`}>{item.quantity} left</p>
                         : (item.quantity && item.quantity <= 20
                             ? <p className={`${classes["item-quantity"]} ${classes["level-medium"]}`}>{item.quantity} left</p>
                             : <p className={`${classes["item-quantity"]} ${classes["level-high"]}`}>{item.quantity} left</p>
                         )
-                    }
+                    } */}
                 </div>
             </div>      
-        )    
-    );  
+        )
+    );
 
   return (
     <div className={classes["item-list"]} >{dataItems}</div>
